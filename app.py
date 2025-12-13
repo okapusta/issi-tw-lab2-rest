bd5a677d7a0375f16a9be111bdec4ae8f80c2d1e
Oskar Kapusta <kapuszasty@gmail.com>
2025-12-13 13:30:53 +0100

    New movie form

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from lib.database_manager import DatabaseManager
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods=['GET'])
+def home():
     return render_template("home.html", movies=__fetch_movies())
+
+@app.route("/movies/new", methods=['GET'])
+def new_movie():
+    return render_template("new.html")
 
 def __fetch_movies():
     manager = DatabaseManager(dataclass=Movie)
