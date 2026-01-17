3ac5c0a3d8452053c52cba6f16d280a1cfe594cc
Oskar Kapusta <kapuszasty@gmail.com>
2026-01-17 13:39:51 +0100

    CORS

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import heathcheck_router, calculator_router, geolocator_router, movie_router, actor_router
 
+origins = [
+    "http://localhost:8888",
+    "*" # Yuck :)
+]
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(heathcheck_router, tags=["healthcheck"])
 app.include_router(calculator_router, tags=["calculator"])
 app.include_router(geolocator_router, tags=["geolocator"])
